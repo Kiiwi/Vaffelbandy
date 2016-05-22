8758b4248376939cefb62bf442210524110ef2a9
Kiiwi <tommy.lee.ryan@gmail.com>
2016-05-22 23:39:05 +0200

    Added production database settings and fixed #rules formatting

--- vaffelbandy/settings.py
+++ vaffelbandy/settings.py
@@ -81,13 +81,14 @@ USE_TZ = True
 
 
 # Parse database configuration from $DATABASE_URL
-#DATABASES['default'] = dj_database_url.config()
+DATABASES['default'] = dj_database_url.config()
 
 # Enable Connection Pooling (if desired)
-#DATABASES['default']['ENGINE'] = 'django_postgrespool'
+DATABASES['default']['ENGINE'] = 'django_postgrespool'
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
 
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
